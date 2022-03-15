88b491165cb0e452409ac21e34a91171b2517ea5
ThanhHieuDang0706 <hieu.dang.dth_is_me@hcmut.edu.vn>
2022-03-15 16:25:32 +0700

    add total-payment-button

--- cart.js
+++ cart.js
@@ -1,7 +1,6 @@
 /* CartItem: Id id of product,
         name : string
         size: array of string that can be: {S, M , L , XL , XLL}  */
-
 class CartItem {
 
     constructor(id, name, unitPrice, size, imgUrl, quantity, productURL) {
@@ -21,20 +20,21 @@ class CartItem {
 
 
 }
-class Cart {
-    constructor(cartItems) {
-        this.cartItems = cartItems;
-    }
+// class Cart {
+//     constructor(cartItems) {
+//         this.cartItems = cartItems;
+//     }
 
-    calculateTotalPrice() {
-        let totalPrice = 0;
-        this.cartItems.forEach(item => {
-            totalPrice += item.totalPrice;
-        })
-        return totalPrice;
-    }
-}
+//     calculateTotalPrice() {
+//         let totalPrice = 0;
+//         this.cartItems.forEach(item => {
+//             totalPrice += item.totalPrice;
+//         })
+//         return totalPrice;
+//     }
+// }
 
+// example data in customer's cart! NOT REAL DATA.
 const cart_data = [
     new CartItem(
         0,
@@ -54,8 +54,20 @@ const cart_data = [
         '#', // product URL
     )
 ]
+
+function getTotalPrice() {
+    let totalPrice = 0;
+    cart_data.forEach(item => {
+        totalPrice += item.totalPrice;
+    })
+    return totalPrice;
+}
+
 if (cart_data) {
     genCartTable();
+    genTotalCart();
+} else {
+    genEmptyCart();
 }
 
 function genTableHeading() {
@@ -224,4 +236,51 @@ function genCartTable() {
     table.append(table_head, table_body);
 
     cartContent.appendChild(table);
+}
+
+function genTotalCart() {
+    let total = document.getElementById('total');
+
+    let divTotalCart = document.createElement('div');
+    divTotalCart.className = "total-cart";
+
+    let orderInfo = document.createElement('p');
+    orderInfo.className = "order-info";
+    orderInfo.innerHTML = "Tổng tiền: "
+
+    let spanPrice = document.createElement('span');
+    spanPrice.className = "total_price";
+
+    let bTagNumber = document.createElement('b');
+    bTagNumber.innerHTML = getTotalPrice();
+
+    spanPrice.appendChild(bTagNumber);
+    orderInfo.appendChild(spanPrice);
+
+    let divPaymentButton = document.createElement('div');
+    divPaymentButton.className = "cart-buttons";
+
+    let paymentButton = document.createElement('button');
+    paymentButton.type = 'submit';
+    paymentButton.id = 'checkout';
+    paymentButton.className = "btn btn-dark"
+    paymentButton.name = "checkout";
+    paymentButton.innerHTML = 'Thanh toán';
+    divPaymentButton.appendChild(paymentButton);
+
+    let divCheckoutNote = document.createElement('div');
+    divCheckoutNote.className = "checkout-note clearfix";
+
+    let textAreaNote = document.createElement('textarea');
+    textAreaNote.id = "note";
+    textAreaNote.name = "note";
+    textAreaNote.placeholder = "Ghi chú";
+    divCheckoutNote.appendChild(textAreaNote);
+
+    divTotalCart.append(orderInfo, divPaymentButton, divCheckoutNote);
+    total.appendChild(divTotalCart);
+}
+
+function genEmptyCart() {
+    console.log("Empty cart");
 }
